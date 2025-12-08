d80ef4426f659770207ad1c0807e9551ad48bae5
rsvn <rizwanshaz2004@gmail.com>
2025-12-08 19:55:24 +0530

    day3-part2:not completed

--- 2025/day3/escalator.py
+++ 2025/day3/escalator.py
@@ -1,92 +1,96 @@
 #converting int to the dict with index as keys
-ls=[]
-result=[]
 sum=0
-
-with open("input.txt","r") as f:
+result=[]
+with open("sample.txt","r") as f:
     inputs= [line.strip() for line in f]
 
 
 
 
-print(inputs)
-
 def digits(n):
-    n=int(n)
-    dg={}
-    index=len(str(n))-1
-    global ls
-    ls=[]
-    while index>=0:
-        dg[n%10]=index
-        ls.append(n%10)
-        n//=10
-        index-=1
-    ls=ls[::-1]
-    return dg 
+    n = str(n)
+    inx_dict = {i: int(n[i]) for i in range(len(n))}
+    ls = [int(x) for x in n]
+    return inx_dict, ls
 
-
-#gets the max digit from the given list
-def get_elements(n):
-    n_dict=digits(n)
-    sorted_ls=sorted(ls,reverse=True)
-
-    index_ls=[n_dict[sorted_ls[0]],n_dict[sorted_ls[1]]]
-    index_ls.sort()
-    if ls[index_ls[0]]>ls[index_ls[1]] or index_ls[1]==len(ls)-1:
-        print(str(ls[index_ls[0]])+str(ls[index_ls[1]]) ,index_ls,n)
-        return result.append((str(ls[index_ls[0]])+str(ls[index_ls[1]])))
-    else:
-        first_dg=ls[index_ls[1]]
-        sliced_ls=ls[index_ls[1]+1:]
-        max=sliced_ls[0]
-        for i in sliced_ls:
-            if max<i:
-                max=i
-        print(first_dg,max)
-        return result.append(str(first_dg)+str(max))
+def get_max(inx_dict,start,end):
+    max_val=-1
+    idx=None
+    for x in range(start,end+1):
+        if x in inx_dict:
+            ## handle the edge case when to skip the same values and when to not 
+            if max_val<=inx_dict[x] :
+                max_val=inx_dict[x]
+                idx=x 
+    #print("index",idx,"max val : ",max_val)
+    return idx 
 
 
 
 
-for i in range(len(inputs)):
-    get_elements(inputs[i])
+
+def get_number(n,n_digits):
+    inx_dict, ls = digits(n)
+    ls_1 = ls[:]  # local copy
+    indices=[]
+    max_idx=-1
+    while n_digits>0:
+        range_max=max(inx_dict)
+        range_min=max_idx
+        
+        if max_idx>=max(inx_dict):
+            range_min=min(indices)
+            print("first")
+            if range_min>range_max:
+                print("second")
+                range_min=min(inx_dict)
+                print(range_min) 
+        print("range:",range_min,max(inx_dict))
+        max_idx=get_max(inx_dict,range_min,range_max)
+        print("index:",max_idx,"val :",inx_dict[max_idx])
+        del inx_dict[max_idx]
+        print(inx_dict)
+        indices.append(max_idx)
+         
 
 
-for i in range(len(result)):
-    sum+=int(result[i])
+        n_digits-=1 
+    return indices,ls_1 
 
-print(result)
+
+def inx_val(indices,ls):
+    val=[]
+    indices.sort()
+    for x in indices:
+        val.append(ls[x])
+    num = int("".join(str(x) for x in val))
+    return num 
+
+
+
+
+#for n in inputs:
+#    inx=get_number(n,12)
+#    print(inx)
+#    num=inx_val(inx)
+#    result.append(num)
+#    print(result)
+
+
+for n in inputs:
+    inx,ls=get_number(n,12)
+    num=inx_val(inx,ls)
+    sum+=int(num)
+    result.append(num)
+    print("for input: ",n,"largest num :",num,"\n")
+
 print(sum)
 
-            
-        
-        
 
-        
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#n='234234234234278'    
+#inx,ls=get_number(n,12)
+#num=inx_val(inx,ls)
+#sum+=int(num)
+#result.append(num)
+#print("for input: ",n,"largest num :",num,"\n")
 
